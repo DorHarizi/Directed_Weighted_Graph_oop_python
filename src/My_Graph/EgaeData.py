37bcaaf5e9563bd37927df38a3ef2c79c0093497
Dor Harizi <dorharizi2@gmail.com>
2021-12-22 12:23:35 +0200

    Dorharizi 22/12/2021 12:23

--- src/My_Graph/EgaeData.py
+++ src/My_Graph/EgaeData.py
@@ -11,7 +11,7 @@ class EdgeData:
         return '{"src" =%d,"dest"=%d, "wight"=%s}' % (self.src, self.dest, self.weight)
 
     """
-    Return the EdgeData src
+    Return the EdgeDadggta src
     """
     def get_src(self):
         return self.src
